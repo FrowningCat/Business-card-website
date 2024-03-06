c3bd64e14618e62cd9c2bc7a3a03b4f5f758b37b
FrowningCat <vsavinkov60@gmail.com>
2024-03-06 15:54:31 +0300

    add 'education' block

--- src/components/LandingPage/Main/Main.jsx
+++ src/components/LandingPage/Main/Main.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Route, Router, Routes } from 'react-router-dom';
 import Items from './Items/Items';
 import CertainItems from './CertainItems/CertainItems';
+import chevron from '../../../assets/img/chevron.png';
 import './main.css';
 
 export default function Header() {
@@ -34,6 +35,74 @@ export default function Header() {
                             element={<Items items={items} />}
                         ></Route>
                     </Routes>
+                </div>
+            </div>
+            <div className="main__education">
+                <h5 className="main__education__title">Образование</h5>
+                <div className="main__education__faqDetails">
+                    <details>
+                        <summary>
+                            <p>
+                                Московский государственный
+                                психолого-педагогический университет
+                            </p>
+                            <img
+                                src={chevron}
+                                alt="chevron"
+                                className="chevron link"
+                            />
+                        </summary>
+                        <p>2020-2020</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Nesciunt odio nobis ratione, minus impedit
+                            debitis quam facere reiciendis perspiciatis cum
+                            iusto placeat culpa accusantium dolores harum ab
+                            totam pariatur soluta. Nulla, corrupti? Repudiandae
+                            perspiciatis aliquam similique voluptas. Iure,
+                            officiis deleniti.
+                        </p>
+                    </details>
+                    <details>
+                        <summary>
+                            <p>GeekBrains</p>
+                            <img
+                                src={chevron}
+                                alt="chevron"
+                                className="chevron link"
+                            />
+                        </summary>
+                        <p>2020-2020</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Nesciunt odio nobis ratione, minus impedit
+                            debitis quam facere reiciendis perspiciatis cum
+                            iusto placeat culpa accusantium dolores harum ab
+                            totam pariatur soluta. Nulla, corrupti? Repudiandae
+                            perspiciatis aliquam similique voluptas. Iure,
+                            officiis deleniti.
+                        </p>
+                    </details>
+                    <details>
+                        <summary>
+                            <p>Udemy</p>
+                            <img
+                                src={chevron}
+                                alt="chevron"
+                                className="chevron link"
+                            />
+                        </summary>
+                        <p>2020-2020</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Nesciunt odio nobis ratione, minus impedit
+                            debitis quam facere reiciendis perspiciatis cum
+                            iusto placeat culpa accusantium dolores harum ab
+                            totam pariatur soluta. Nulla, corrupti? Repudiandae
+                            perspiciatis aliquam similique voluptas. Iure,
+                            officiis deleniti.
+                        </p>
+                    </details>
                 </div>
             </div>
         </div>
